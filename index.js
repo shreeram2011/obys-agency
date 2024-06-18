7275ea459aa110683e02712a638e29751035b99b
Shreeram Yadav <yadavshreeram147@gmail.com>
2024-06-18 17:31:22 +0530

    Video effect successfully done

--- index.js
+++ index.js
@@ -94,11 +94,56 @@ function cursorAnimation(){
         duration: 1
     })
     Shery.makeMagnet("#nav-part2 h4"); 
+
+    var videoContainer = document.querySelector("#video-container");
+    var video = document.querySelector("#video-container video");
+    videoContainer.addEventListener("mouseenter",function(){
+        videoContainer.addEventListener("mousemove",function(dets){
+            gsap.to(".mousefollower",{
+                opacity : 0
+            })
+            gsap.to("#video-cursor",{
+                left : dets.x - 500,
+                y : dets.y - 200
+            })
+        })
+        videoContainer.addEventListener("mouseleave",function(){
+            gsap.to(".mousefollower",{
+                opacity : 1
+            })
+            gsap.to("#video-cursor",{
+                left : "80%",
+                top : "-20%"
+            })
+        })
+
+        var flag = 0;
+        videoContainer.addEventListener("click",function(){
+            if(flag == 0){
+                video.play();
+                video.style.opacity = 1;
+                document.querySelector("#video-cursor").innerHTML = `<i class="ri-pause-mini-fill"></i>`;
+                gsap.to("#video-cursor",{
+                    scale : 0.5
+                })
+                flag = 1;
+            }
+            else{
+                video.pause();
+                video.style.opacity = 0;
+                document.querySelector("#video-cursor").innerHTML = `<i class="ri-play-mini-fill"></i>`;
+                gsap.to("#video-cursor",{
+                    scale : 1
+                })
+                flag = 0;
+            }
+        })
+    })
 }
 function sheryjsanimation(){
     Shery.imageEffect(".image-div",{
-        style : 5,
-        config : {"a":{"value":2,"range":[0,30]},"b":{"value":0.75,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7812619017077115},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.76,"range":[0,10]},"metaball":{"value":0.41,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0.03,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
+        style : 6,
+        config : {"noiseDetail":{"value":16.03,"range":[0,100]},"distortionAmount":{"value":1.07,"range":[0,10]},"scale":{"value":47.33,"range":[0,100]},"speed":{"value":0.79,"range":[0,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7812619017077115},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0.11,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.31,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":1.45,"range":[0,10]},"metaball":{"value":0.47,"range":[0,2]},"discard_threshold":{"value":0.53,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":8.4,"range":[0,100]}},
         gooey : true
     })
 }
